66e8c7021bdaa1444c43ad8baa756d4a3c769dd7
Timo Fuckner <timo.fuckner@mailbox.tu-dresden.de>
2016-11-10 16:52:26 +0100

    added signals

--- com.py
+++ com.py
@@ -4,7 +4,36 @@ import time
 import struct
 
 class Mainboard():
-    
+    signal = dict(
+       START_byte    = '\xCC',  #start byte for USART transmission
+       STOP_byte     = '\x1F',  #stop byte for USART transmission
+
+       PRS0_id       = '\x31',  #ID for data of PRS_sensor_0
+       PRS1_id       = '\x32',  #ID for data of PRS_sensor_1
+       TEMP_id       = '\x33',  #ID for data of TEMP_sensor
+       CAM0_id       = '\x34',  #ID for status of CAM_0
+       CAM1_id       = '\x35',  #ID for status of CAM_1
+       ARM_id        = '\x36',  #ID for status of ARM_sensor
+       RF_id         = '\x37',  #ID for status of RF-Board
+       VLV_id        = '\x38',  #ID for status of Valve_0
+       RXSM_id       = '\x30',  #ID for RXSM signal reception answer
+
+       GET_prs0      = '\x61',  #ID for request of single PRS0 value
+       GET_prs1      = '\x62',  #ID for request of single PRS1 value
+       GET_temp      = '\x63',  #ID for request of single TEMP value
+       GET_arm       = '\x66',  #ID for request of arm state
+       OPN_vlv       = '\x67',  #ID for command to open Valve_0
+       CLS_vlv       = '\x68',  #ID for command to close Valve_0
+       REQ_pwr_dwn   = '\x6F',  #ID for power down request
+
+       CAM0_ok       = '\xA4',  #ID for cam0 status ok
+       CAM1_ok       = '\xA5',  #ID for cam1 status ok
+       ARM_ok        = '\xA6',  #ID for ARM sensor ok
+       RF_ok         = '\xA7',  #ID for RF board ok
+       VLV_opnd      = '\xA8',  #ID for Valve_0 opened
+       VLV_clsd      = '\xA9',  #ID for Valve_0 closed
+       ERR_stat      = '\xA0',  #ID for error status
+    )
     def __init__(self, port="/dev/ttyUSB0"):
         self.baud     = 38400
         self.databits = 8
@@ -28,9 +57,9 @@ class Mainboard():
             previous_byte = byte
             byte = self.rs232.read(1)
 #            print ''.join( [ "%02X " % ord( x ) for x in byte ] ).strip()
-            if byte == chr(0xCC):
+            if byte == signal["START_byte"]:
                 #print "FOUND FIRST START BYTE"
-                if previous_byte == chr(0xCC):
+                if previous_byte == signal["START_byte"]:
                     #print "FOUND SECOND START BYTE"
                     break
 
@@ -40,11 +69,21 @@ class Mainboard():
         #print "PACKAGELENGTH DETECTED: " + str(packagelength)
         #print "#" + str(packagenumber) + ": " + data
         readabledata = ''.join( [ "%02X " % ord( x ) for x in data ] )#.strip()
+        
+        unpackcounter = 0
+        datathing = []
+        while True:
+            try:
+                datathing.append(struct.unpack_from("<B",data,offset=unpackcounter)[0])
+                unpackcounter=unpackcounter+1
+            except:
+                break
+        
         print "#"+str(packagenumber) + ": " + readabledata 
         stop = self.rs232.read(2)
         #print stop
         #print ''.join( [ "%02X " % ord( x ) for x in stop ] ).strip()
-        if (stop != struct.pack("<H",0x1F1F)):
+        if (stop != 2*signal["STOP_byte"]):
             print "ERROR: Package number " + str(packagenumber) + " is broken!\n"
         
 
